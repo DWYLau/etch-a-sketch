0da0724dbd7d8a873025472a6f0f8066366bcc2b
Derek <dereklau15@gmail.com>
2022-06-25 23:44:38 +0900

    Update buttons, add rainbow, color modes, rework code

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
-Etch-a-Sketch
-
 Etch-a-Sketch Project
 
 Final thoughts:
 
 A complete different project compared to RPS.
 
-RPS included things like selections and 3 different choices, but this project worked with things like numbers and grids.
+RPS included user inputs and getting computer outputs, but this project worked with things like grids and DOM manipulation whilst also trying to get user input to change the size of the grid.
 
 The most difficult part of this project was the implemention of the user input to create a new grid.
 
@@ -15,3 +13,9 @@ I did try using a button to prompt for user input however it just did not work a
 Through some research, I found that you can use a HTML input element to get user inputs so I tried that instead.
 
 That worked amazingly so I'm pretty happy with that.
+
+Update: I reworked some code after looking at a tutorial. At first, this project only had a black mouseover color and a reset button.
+
+After the tutorial, I added an eraser and rainbow mode whilst also reworking the reset button's event listener to be more clear.
+
+Additionally, after looking at other people's work, I decided to research and add a color button where you can select your own color!
--- scripts.js
+++ scripts.js
@@ -1,45 +1,51 @@
-const grid = document.querySelector('.grid');
-const square = document.querySelector('div');
-const resetButton = document.querySelector('.resetButton')
-const userInput = document.querySelector('.gridSize');
+let color = 'black';
 
-function createGrid() {
-    for (let i = 0; i < 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        grid.appendChild(div);
+function createGrid(size) {
+    const grid = document.querySelector('.grid');
+    const gridSquares = grid.querySelectorAll('div');
+    gridSquares.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    let gridSize = size * size;
+    for (let i = 0; i < gridSize; i++) {
+        const gridSquare = document.createElement('div');
+        gridSquare.addEventListener('mouseover', colorSquare);
+        gridSquare.style.backgroundColor = 'white';
+        grid.appendChild(gridSquare);
     }
 }
 
-function newGrid() {
-    grid.innerHTML = "";
-    grid.style.setProperty(
-        "grid-template-columns",
-        `repeat(${userInput.value}, 1fr)`
-    );
-    grid.style.setProperty(
-        "grid-template-rows",
-        `repeat(${userInput.value}, 1fr)`
-    );
-    for (let i = 0; i < userInput.value * userInput.value; i++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        grid.appendChild(div);
+createGrid(16);
+
+function newGrid(input) {
+    if (input >= 2 && input <= 100) {
+        document.querySelector('.error').style.display = 'none';
+        createGrid(input);
+    }   else {
+        document.querySelector('.error').style.display = 'flex';
+    }
+}
+
+function colorSquare() {
+    if (color === 'rainbow') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    }   else {
+        this.style.backgroundColor = color;
     }
 };
 
-userInput.addEventListener('change', newGrid)
+function changeColor(colorChoice) {
+    color = colorChoice;
+}
 
-grid.addEventListener('mouseover', function(event) {
-    event.target.classList.replace('square', 'color');
+document.querySelector('#input-color').addEventListener('input', function(e) {
+    color = e.target.value;
 });
 
-resetButton.addEventListener('click', function() {
-    grid.innerHTML = "";
-    userInput.valie = "";
-    grid.style.setProperty("grid-template-columns", `repeat(16, 1fr)`);
-    grid.style.setProperty("grid-template-rows", `repeat(16, 1fr)`);
-    createGrid();
+document.querySelector('.resetButton').addEventListener('click', function() {
+    const grid = document.querySelector('.grid');
+    const gridSquares = grid.querySelectorAll('div');
+    gridSquares.forEach((div) => (div.style.backgroundColor = 'white'));
+    color = 'black';
 });
-
-createGrid()
